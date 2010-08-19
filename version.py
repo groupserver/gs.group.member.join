18de0a3fdc041a1a75e5f32971bfedaeafb5b27f
Michael JasonSmith <mpj17@onlinegroups.net>
2010-08-19 10:46:33 +1000

    New version of version.py

--- version.py
+++ version.py
@@ -1,22 +1,61 @@
 version='1.0'
 release=False
 
-import commands, glob, os
-def get_version():
+#-------------------------------------------------------------------------------------#
+
+import commands
+import datetime
+import os
+import glob
+
+class CommandError(Exception):
+    pass
+
+def execute_command(commandstring):
+    status, output = commands.getstatusoutput(commandstring)
+    if status != 0:
+        raise CommandError
+    return output
+
+def parse_version_from_package():
+    try:
+        pkginfo = os.path.join(glob.glob('*.egg-info')[0],
+                                         'PKG-INFO')
+    except:
+        pkginfo = ''
+    
     version_string = ''
-    if not release:
-        status,output = commands.getstatusoutput("hg tip --template '{node|short}'")
-        if status != 0:
-            pkginfo = os.path.join(glob.glob('*.egg-info')[0],
-                                   'PKG-INFO')
-            if os.path.exists(pkginfo):
-                for line in file(pkginfo):
-                    if line.find('Version: ') == 0:
-                        version_string = line.strip().split('Version: ')[1].strip()
-            if not version_string:
-                version_string = '%s-dev' % version
-        else:
-            version_string += '%s-%s' % (version, output)
+    if os.path.exists(pkginfo):
+        for line in file(pkginfo):
+            if line.find('Version: ') == 0:
+                version_string = line.strip().split('Version: ')[1].strip()
+        if not version_string:
+            version_string = '%s-dev' % version
+    else:
+        version_string = version
     
     return version_string
+
+def get_version():
+    try:
+        globalid = execute_command("hg identify -i")
+        commitdate = execute_command("hg log -r %s --template '{date|isodatesec}'" % globalid)
+        # convert date to UTC unix timestamp, using the date command because python
+        # date libraries do not stabilise till about 2.6
+        timestamp = int(execute_command('date -d"%s" --utc +%%s' % commitdate))
+    
+        # finally we have something we can use!
+        dt = datetime.datetime.utcfromtimestamp(timestamp)
+        datestring = dt.strftime('%Y%m%d%H%M%S')
+
+        version_string = "%s-%s-%s" % (version, datestring, globalid)
+
+    except CommandError, IntegerError:
+        version_string = parse_version_from_package()
+
+    return version_string
+
+if __name__ == '__main__':
+    print get_version()
+
 
