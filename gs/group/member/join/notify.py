81936a27fe04ed1295acd18c87d2d03f82087df8
Michael JasonSmith <mpj17@onlinegroups.net>
2012-01-10 16:13:17 +1100

    Moving the notify messages to their own class.

--- gs/group/member/join/notify.py
+++ gs/group/member/join/notify.py
@@ -1,10 +1,7 @@
 # coding=utf-8
-from urllib import quote
 from zope.component import createObject, getMultiAdapter
 from zope.cachedescriptors.property import Lazy
-from gs.group.base.page import GroupPage
 from gs.profile.notify.sender import MessageSender
-from Products.GSGroup.interfaces import IGSMailingListInfo
 UTF8 = 'utf-8'
 
 class NotifyNewMember(object):
@@ -41,29 +38,4 @@ class NotifyNewMember(object):
         html = self.htmlTemplate(userInfo=userInfo)
         ms = MessageSender(self.context, userInfo)
         ms.send_message(subject, text, html)
-
-class NotifyMemberMessage(GroupPage):
-    @Lazy
-    def email(self):
-        l = IGSMailingListInfo(self.groupInfo.groupObj)
-        retval = l.get_property('mailto')
-        return retval
-    
-    @Lazy
-    def supportEmail(self):
-        msg = u'Hi!\n\nI am a member of the group %s\n    %s\nand...' % \
-            (self.groupInfo.name, self.groupInfo.url)
-        sub = quote('Group Welcome')
-        retval = 'mailto:%s?Subject=%s&body=%s' % \
-            (self.siteInfo.get_support_email(), sub, quote(msg.encode(UTF8)))
-        return retval
-        
-class NotifyMemberMessageText(NotifyMemberMessage):
-    def __init__(self, context, request):
-        NotifyMemberMessage.__init__(self, context, request)
-        response = request.response
-        response.setHeader("Content-Type", 'text/plain; charset=UTF-8')
-        filename = 'welcome-to-%s.txt' % self.groupInfo.name
-        response.setHeader('Content-Disposition',
-                            'inline; filename="%s"' % filename)
 
---
+++ gs/group/member/join/notifymessages.py
@@ -0,0 +1,59 @@
+# coding=utf-8
+from urllib import quote
+from zope.cachedescriptors.property import Lazy
+from gs.group.base.page import GroupPage
+from Products.GSGroup.interfaces import IGSMailingListInfo
+UTF8 = 'utf-8'
+
+class NotifyMemberMessage(GroupPage):
+    @Lazy
+    def email(self):
+        l = IGSMailingListInfo(self.groupInfo.groupObj)
+        retval = l.get_property('mailto')
+        return retval
+    
+    @Lazy
+    def supportEmail(self):
+        msg = u'Hi!\n\nI am a member of the group %s\n    %s\nand...' % \
+            (self.groupInfo.name, self.groupInfo.url)
+        sub = quote('Group Welcome')
+        retval = 'mailto:%s?Subject=%s&body=%s' % \
+            (self.siteInfo.get_support_email(), sub, quote(msg.encode(UTF8)))
+        return retval
+        
+class NotifyMemberMessageText(NotifyMemberMessage):
+    def __init__(self, context, request):
+        NotifyMemberMessage.__init__(self, context, request)
+        response = request.response
+        response.setHeader("Content-Type", 'text/plain; charset=UTF-8')
+        filename = 'welcome-to-%s.txt' % self.groupInfo.name
+        response.setHeader('Content-Disposition',
+                            'inline; filename="%s"' % filename)
+
+# And the equivilent message that is sent to the administrators.
+
+class NotifyAdminMessage(GroupPage):
+    @Lazy
+    def email(self):
+        l = IGSMailingListInfo(self.groupInfo.groupObj)
+        retval = l.get_property('mailto')
+        return retval
+    
+    @Lazy
+    def supportEmail(self):
+        msg = u'Hi!\n\nI am an administrator of the group %s\n    %s\nand...' % \
+            (self.groupInfo.name, self.groupInfo.url)
+        sub = quote('New Member')
+        retval = 'mailto:%s?Subject=%s&body=%s' % \
+            (self.siteInfo.get_support_email(), sub, quote(msg.encode(UTF8)))
+        return retval
+        
+class NotifyMemberMessageText(NotifyMemberMessage):
+    def __init__(self, context, request):
+        NotifyMemberMessage.__init__(self, context, request)
+        response = request.response
+        response.setHeader("Content-Type", 'text/plain; charset=UTF-8')
+        filename = 'new-member-%s.txt' % self.groupInfo.name
+        response.setHeader('Content-Disposition',
+                            'inline; filename="%s"' % filename)
+
