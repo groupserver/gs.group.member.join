559ffff86fae5877480e73d7a1ddebb9789b7088
richard@onlinegroups.net <richard@onlinegroups.net>
2012-06-22 14:21:44 +1200

    Update to SQLAlchemy

--- gs/group/member/join/audit.py
+++ gs/group/member/join/audit.py
@@ -179,7 +179,7 @@ class JoinAuditor(object):
     @property
     def queries(self):
         if self.__queries == None:
-            self.__queries = AuditQuery(self.context.zsqlalchemy)
+            self.__queries = AuditQuery()
         return self.__queries
 
     def info(self, code, instanceDatum='', supplementaryDatum=''):
