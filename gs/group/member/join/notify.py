6b6dab08297b3f37c51b7adb13af1f7c558bd5cf
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-20 14:01:42 +1100

    Code cleanup of the notifier class.

--- gs/group/member/join/notify.py
+++ gs/group/member/join/notify.py
@@ -5,10 +5,11 @@ from gs.profile.notify.sender import MessageSender
 from gs.profile.email.base.emailuser import EmailUser
 UTF8 = 'utf-8'
 
+
 class NotifyNewMember(object):
     textTemplateName = 'new-member-msg.txt'
     htmlTemplateName = 'new-member-msg.html'
-    
+
     def __init__(self, context, request):
         self.context = context
         self.request = request
@@ -21,18 +22,18 @@ class NotifyNewMember(object):
 
     @Lazy
     def textTemplate(self):
-        retval = getMultiAdapter((self.context, self.request), 
+        retval = getMultiAdapter((self.context, self.request),
                     name=self.textTemplateName)
         assert retval
         return retval
 
     @Lazy
     def htmlTemplate(self):
-        retval = getMultiAdapter((self.context, self.request), 
+        retval = getMultiAdapter((self.context, self.request),
                     name=self.htmlTemplateName)
         assert retval
         return retval
-        
+
     def notify(self, userInfo):
         subject = (u'Welcome to %s' % (self.groupInfo.name).encode(UTF8))
         emailUser = EmailUser(userInfo.user, userInfo)
@@ -41,12 +42,13 @@ class NotifyNewMember(object):
         ms = MessageSender(self.context, userInfo)
         ms.send_message(subject, text, html)
 
+
 # And the equivilent class for telling the admin
 
 class NotifyAdmin(NotifyNewMember):
     textTemplateName = 'new-member-admin-msg.txt'
     htmlTemplateName = 'new-member-admin-msg.html'
-            
+
     def notify(self, adminInfo, userInfo):
         subject = (u'%s: New Member' % (self.groupInfo.name).encode(UTF8))
         emailUser = EmailUser(userInfo.user, userInfo)
@@ -56,4 +58,3 @@ class NotifyAdmin(NotifyNewMember):
                                  userEmail=emailUser)
         ms = MessageSender(self.context, adminInfo)
         ms.send_message(subject, text, html)
-
