82bba9fc733b8dd4887a2227ce48bb6ac673fd76
Michael JasonSmith <mpj17@onlinegroups.net>
2013-04-24 11:01:21 -0700

    Using the group-visibility to determine if a group is public, or not.

--- gs/group/member/join/joinlink.py
+++ gs/group/member/join/joinlink.py
@@ -1,15 +1,15 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 from zope.cachedescriptors.property import Lazy
 from gs.group.member.viewlet import MemberViewlet
+from gs.group.privacy.interfaces import IGSGroupVisibility
 
 
 class JoinLinkViewlet(MemberViewlet):
     def __init__(self, group, request, view, manager):
-        MemberViewlet.__init__(self, group, request, view, manager)
+        super(JoinLinkViewlet, self).__init__(group, request, view, manager)
 
     @Lazy
     def show(self):
-        # Show the Join links if the user is not a member, and the
-        #   group is public.
-        retval = not(self.isMember) and self.viewTopics
+        v = IGSGroupVisibility(self.groupInfo)
+        retval = not(self.isMember) and v.isPublic
         return retval
