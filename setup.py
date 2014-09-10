fa064a749f04b38949166a2e95960bc8f47031b2
Michael JasonSmith <mpj17@onlinegroups.net>
2014-09-09 18:03:49 -0700

    Renaming README.txt to README.rst
    --HG--
    rename : README.txt => README.rst

--- setup.py
+++ setup.py
@@ -17,9 +17,10 @@ import codecs
 import os
 from version import get_version
 
-with codecs.open('README.txt', encoding='utf-8') as f:
+with codecs.open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
-with codecs.open(os.path.join("docs", "HISTORY.txt"), encoding='utf-8') as f:
+with codecs.open(os.path.join("docs", "HISTORY.txt"), 
+                 encoding='utf-8') as f:
     long_description += '\n' + f.read()
 
 version = get_version()
