2d43d6dd46165249a5581c8d1ffb1e210c98440f
Michael JasonSmith <mpj17@onlinegroups.net>
2014-09-12 11:15:17 -0700

    More PEP-8 OCD.

--- gs/group/member/join/event.py
+++ gs/group/member/join/event.py
@@ -1,18 +1,33 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+############################################################################
+#
+# Copyright © 2012, 2013, 2014 OnlineGroups.net and Contributors.
+#
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################
+from __future__ import absolute_import, unicode_literals
 from zope.component.interfaces import ObjectEvent
 from zope.component.interfaces import IObjectEvent
-from zope.interface import Attribute, implements
+from zope.interface import Attribute, implementer
+
 
 class IGSJoinGroupEvent(IObjectEvent):
     """ An event issued after someone has joined a group."""
-    groupInfo   = Attribute(u'The group that is being joined')
-    memberInfo  = Attribute(u'The new group member')
+    groupInfo = Attribute('The group that is being joined')
+    memberInfo = Attribute('The new group member')
 
+
+@implementer(IGSJoinGroupEvent)
 class GSJoinGroupEvent(ObjectEvent):
-    implements(IGSJoinGroupEvent)
-    
     def __init__(self, context, groupInfo, memberInfo):
-        ObjectEvent.__init__(self, context)
+        super(GSJoinGroupEvent, self).__init__(context)
         self.groupInfo = groupInfo
         self.memberInfo = memberInfo
-
