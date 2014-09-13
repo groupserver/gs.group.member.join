f84332fcb411d20468c520bd5ea7e588c60b7c9c
Michael JasonSmith <mpj17@onlinegroups.net>
2014-09-12 17:51:52 -0700

    Using the new notifier abstract base-class, and adding the Cannot Join and Already Confirmed notifiers

--- gs/group/member/join/notify.py
+++ gs/group/member/join/notify.py
@@ -13,25 +13,19 @@
 #
 ############################################################################
 from __future__ import unicode_literals
-from zope.component import createObject
-from zope.cachedescriptors.property import Lazy
 from zope.i18n import translate
-from gs.profile.notify import MessageSender, NotifierABC
+from gs.content.email.base import (NotifierABC, GroupNotifierABC,
+                                   AnonymousNotifierABC)
+from gs.email import send_email
 from gs.profile.email.base.emailuser import EmailUser
+from gs.profile.notify import MessageSender
 from . import GSMessageFactory as _
 UTF8 = 'utf-8'
 
 
-class NotifyNewMember(NotifierABC):
+class NotifyNewMember(GroupNotifierABC):
     textTemplateName = 'new-member-msg.txt'
     htmlTemplateName = 'new-member-msg.html'
-
-    @Lazy
-    def groupInfo(self):
-        retval = createObject('groupserver.GroupInfo', self.context)
-        assert retval, 'Could not create the GroupInfo from %s' % \
-            self.context
-        return retval
 
     def notify(self, userInfo):
         subject = _('welcome-subject', 'Welcome to ${groupName}',
@@ -47,26 +41,30 @@ class NotifyNewMember(NotifierABC):
 
 # And the equivilent class for telling the admin
 
-class NotifyAdmin(NotifyNewMember):
+class NotifyAdmin(GroupNotifierABC):
     textTemplateName = 'new-member-admin-msg.txt'
     htmlTemplateName = 'new-member-admin-msg.html'
 
     def notify(self, adminInfo, userInfo):
-        subject = ('%s: New Member' % (self.groupInfo.name).encode(UTF8))
+        subject = _('new-member-subject',
+                    '${userName} has joined ${groupName}',
+                    mapping={'userName': userInfo.name,
+                             'groupName': self.groupInfo.name})
+        translatedSubject = translate(subject)
         emailUser = EmailUser(userInfo.user, userInfo)
         text = self.textTemplate(adminInfo=adminInfo, userInfo=userInfo,
                                  userEmail=emailUser)
         html = self.htmlTemplate(adminInfo=adminInfo, userInfo=userInfo,
                                  userEmail=emailUser)
         ms = MessageSender(self.context, adminInfo)
-        ms.send_message(subject, text, html)
+        ms.send_message(translatedSubject, text, html)
         self.reset_content_type()
 
 
 # The email asking someone to confirm they want to be in a group.
 
 
-class ConfirmationNotifier(NotifyNewMember):
+class ConfirmationNotifier(GroupNotifierABC):
     textTemplateName = 'gs-group-member-join-confirm.txt'
     htmlTemplateName = 'gs-group-member-join-confirm.html'
 
@@ -80,6 +78,48 @@ class ConfirmationNotifier(NotifyNewMember):
         emailUser = EmailUser(userInfo.user, userInfo)
         text = self.textTemplate(userInfo=userInfo, userEmail=emailUser)
         html = self.htmlTemplate(userInfo=userInfo, userEmail=emailUser)
+        ms = MessageSender(self.context, userInfo)
+        ms.send_message(translatedSubject, text, html)
+        self.reset_content_type()
+
+
+# The email telling someone that they cannot be in the group
+
+
+class NotifyCannotJoin(AnonymousNotifierABC):
+    textTemplateName = 'gs-group-member-join-refuse.txt'
+    htmlTemplateName = 'gs-group-member-join-refuse.html'
+
+    def notify(self, addr, groupInfo):
+        subject = _('refuse-subject',
+                    'Cannot join ${groupName}',
+                    mapping={'groupName': self.groupInfo.nam})
+        translatedSubject = translate(subject)
+        text = self.textTemplate(emailAddress=addr, groupInfo=groupInfo)
+        html = self.htmlTemplate(emailAddress=addr, groupInfo=groupInfo)
+
+        fromAddr = self.fromAddr(groupInfo.siteInfo)
+        message = self.create_message(addr, fromAddr, translatedSubject,
+                                      text, html)
+        send_email(groupInfo.siteInfo.get_support_email(), addr, message)
+        self.reset_content_type()
+
+
+# The email telling the member that he or she has already confirmed
+
+
+class NotifyConfirmed(NotifierABC):
+    textTemplateName = 'gs-group-member-join-confirmed.txt'
+    htmlTemplateName = 'gs-group-member-join-confirmed.html'
+
+    def notify(self, userInfo, groupInfo):
+        subject = _('confirmed-subject',
+                    'Already joined ${groupName}',
+                    mapping={'groupName': self.groupInfo.nam})
+        translatedSubject = translate(subject)
+        text = self.textTemplate(groupInfo=groupInfo)
+        html = self.htmlTemplate(groupInfo=groupInfo)
+
         ms = MessageSender(self.context, userInfo)
         ms.send_message(translatedSubject, text, html)
         self.reset_content_type()
