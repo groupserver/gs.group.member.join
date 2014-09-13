85f38994672457067071e412aa3a713879e61e44
Michael JasonSmith <mpj17@onlinegroups.net>
2014-09-12 17:52:06 -0700

    Doing more checks on joining.

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -4,26 +4,30 @@ Changelog
 4.0.0 (2014-09-xx)
 ------------------
 
-* Added internationalisation
-* Update of the notifications.
-* Added support for the ``subscribe`` email-command
+* Added support for the *Subscribe* email-command
   
-  + Added the command adaptor
-  + Added the *Confirm subscription* notifications
-  + Added the confirm subscription command adaptor.
+  + The ``subscribe`` command adaptor
+  + Support for groups in public-to-site-member sites, as well
+    normal public groups
+  + The ``confirmation`` SQL table
+  + The *Confirm subscription* notification
+  + The ``confirm`` command adaptor
+
+* Added internationalisation
+* Update of the notifications
 
 3.2.2 (2014-06-13)
 ------------------
 
 * Switching to Unicode literals
-* Following the form base-class to ``gs.content.form.base``.
+* Following the form base-class to ``gs.content.form.base``
 
 3.2.1 (2014-02-28)
 ------------------
 
 * Ensure the headers are in ASCII.
 * Ensure the old ``Content-type`` is set after the notifications
-  are sent.
+  are sent
 
 3.2.0 (2013-10-11)
 ------------------
@@ -35,7 +39,7 @@ Changelog
 3.1.2 (2013-07-17)
 ------------------
 
-* Fixing the ``Content-type`` header.
+* Fixing the ``Content-type`` header
 
 3.1.1 (2013-05-28)
 ------------------
@@ -51,15 +55,15 @@ Changelog
 ------------------
 
 * Update for the new UI
-* Changing the marker-interface that is used.
+* Changing the marker-interface that is used
 * Code cleanup
 
 
 2.1.1 (2013-01-22)
 ------------------
 
-* Making links relative to the base of the site.
-* Updating where the Admin link goes.
+* Making links relative to the base of the site
+* Updating where the Admin link goes
 
 2.1.0 (2012-08-02)
 ------------------
@@ -70,7 +74,7 @@ Changelog
 2.0.1 (2012-02-22)
 ------------------
 
-* Fix for the ``join`` event.
+* Fix for the ``join`` event
 
 2.0.0 (2012-01-11)
 ------------------
@@ -87,7 +91,7 @@ Changelog
 
 * Use the standard code for displaying feedback messages
 * Handle corner cases better
-* Switch to a standard ``GroupForm`` for the Join page.
+* Switch to a standard ``GroupForm`` for the Join page
 
 1.1.0 (2010-11-25)
 ------------------
@@ -97,20 +101,20 @@ Changelog
 1.0.3 (2010-10-07)
 ------------------
 
-* Following the radio-button code.
+* Following the radio-button code
 
 1.0.2 (2010-08-12)
 ------------------
 
 * Better handling of Anonymous
-* Fixing the ``context``.
+* Fixing the ``context``
 
 1.0.1 (2010-08-02)
 ------------------
 
-* Adding the audit-event factory.
+* Adding the audit-event factory
 
 1.0.0 (2010-07-29)
 ------------------
 
-* Initial release, moving the page here from ``Products.GSGroupMember``.
+* Initial release, moving the page here from ``Products.GSGroupMember``
--- gs/group/member/join/listcommand.py
+++ gs/group/member/join/listcommand.py
@@ -21,9 +21,10 @@ from zope.component import createObject
 from zope.cachedescriptors.property import Lazy
 from gs.core import to_id
 from gs.group.list.command import CommandResult, CommandABC
-from gs.group.member.base import user_member_of_group
+from gs.group.member.base import (user_member_of_group, user_member_of_site)
+from gs.group.privacy.visibility import GroupVisibility
 from Products.GSProfile.utils import create_user_from_email
-from .notify import ConfirmationNotifier
+from .notify import (ConfirmationNotifier, NotifyCannotJoin)
 from .queries import ConfirmationQuery
 from .utils import join
 
@@ -60,9 +61,9 @@ class SubscribeCommand(CommandABC):
         'Process the email command ``subscribe``'
         addr = self.get_addr(email)
         userInfo = self.get_userInfo(addr)
+        groupVisibility = GroupVisibility(self.groupInfo)
         if userInfo and user_member_of_group(userInfo, self.group):
             # We are dealing with an existing user.
-            retval = CommandResult.notACommand
             m = 'Ignorning the "subscribe" command from {user.name} '\
                 '({user.id}) <{addr}> because the person is already a '\
                 'member of {group.name} ({group.id}) on {site.name} '\
@@ -70,19 +71,38 @@ class SubscribeCommand(CommandABC):
             msg = m.format(user=userInfo, addr=addr, group=self.groupInfo,
                            site=self.groupInfo.siteInfo)
             log.info(msg)
-        else:  # Not a member of the group, one way or the other.
+            retval = CommandResult.notACommand
+        elif groupVisibility.isPublic:
+            # The group is public, so the person making the request can
+            # join.
             if not userInfo:
                 userInfo = self.create_user(email['From'])
-            # Generate the confirmation ID
-            confirmationId = self.generate_confirmation_id(email)
-            self.query.add_confirmation(
-                addr, confirmationId, userInfo.id, self.groupInfo.id,
-                self.groupInfo.siteInfo.id)
-            # Send the notification
-            notifier = ConfirmationNotifier(self.group, request)
-            notifier.notify(userInfo, confirmationId)
+            self.send_confirmation(email, addr, userInfo, request)
+            retval = CommandResult.commandStop
+        elif (userInfo and not(user_member_of_group(userInfo, self.group))
+              and groupVisibility.isPublicToSite
+              and user_member_of_site(userInfo, self.groupInfo.siteInfo)):
+            # The group is public to *site* *members* and the person sending
+            # the request is a site member
+            self.send_confirmation(email, addr, userInfo, request)
+            retval = CommandResult.commandStop
+        else:
+            # TODO: Send a Cannot Join notification
+            groupsFolder = self.group.groupObj.aq_parent
+            notifier = NotifyCannotJoin(groupsFolder, request)
+            notifier.notify(addr, self.groupInfo)
             retval = CommandResult.commandStop
         return retval
+
+    def send_confirmation(self, email, addr, userInfo, request):
+        # Generate the confirmation ID
+        confirmationId = self.generate_confirmation_id(email)
+        self.query.add_confirmation(
+            addr, confirmationId, userInfo.id, self.groupInfo.id,
+            self.groupInfo.siteInfo.id)
+        # Send the notification
+        notifier = ConfirmationNotifier(self.group, request)
+        notifier.notify(userInfo, confirmationId)
 
     def create_user(self, fromHeader):
         'Create a user from the From header, setting the ``fn``.'
@@ -127,25 +147,17 @@ class ConfirmCommand(CommandABC):
             confirmationInfo = self.query.get_confirmation(addr,
                                                            confirmationId)
             if confirmationInfo and (confirmationInfo['email'] == addr):
-                # Because the email comes into Support we may be on a
-                # totally different site. Get the correct site as the
-                # context.
-                siteRoot = self.group.site_root()
-                site = getattr(siteRoot.Content, confirmationInfo['siteId'])
-                # Get the user and group with the right context.
-                userInfo = createObject('groupserver.UserFromId', site,
-                                        confirmationInfo['userId'])
-                groupInfo = createObject('groupserver.GroupInfo', site,
-                                         confirmationInfo['groupId'])
-                join(groupInfo.groupObj, request, userInfo, groupInfo)
-                self.query.clear_confirmations(userInfo.id, groupInfo.id)
+                self.join(confirmationInfo, request)
                 retval = CommandResult.commandStop
             elif not confirmationInfo:
+                # Found an "ID-" in the confirmation-email, but no matching
+                # confirmation ID in the database.
                 m = 'No confirmation information found in command from '\
                     '<{addr}>: {subject}'
                 msg = m.format(addr=addr, subject=email['Subject'])
                 log.info(msg)
-                retval = CommandResult.notACommand
+                # TODO: notification
+                retval = CommandResult.commandStop
             else:  # confirmationInfo['email'] != addr
                 m = 'Email address <{addr}> does not match that in the '\
                     'confirmation info <{confirmationAddr}>: {subject}'
@@ -153,11 +165,26 @@ class ConfirmCommand(CommandABC):
                                confirmationAddr=confirmationInfo['email'],
                                subject=email['Subject'])
                 log.info(msg)
-                retval = CommandResult.notACommand
+                # TODO: notification
+                retval = CommandResult.commandStop
         else:
+            # Assume it is a normal email.
             m = 'No confirmation ID found in command from <{addr}>: '\
                 '{subject}'
             msg = m.format(addr=addr, subject=email['Subject'])
             log.info(msg)
             retval = CommandResult.notACommand
         return retval
+
+    def join(self, confirmationInfo, request):
+        # Because the email comes into Support we may be on a totally
+        # different site. Get the correct site as the context.
+        siteRoot = self.group.site_root()
+        site = getattr(siteRoot.Content, confirmationInfo['siteId'])
+        # Get the user and group with the right context.
+        userInfo = createObject('groupserver.UserFromId', site,
+                                confirmationInfo['userId'])
+        groupInfo = createObject('groupserver.GroupInfo', site,
+                                 confirmationInfo['groupId'])
+        join(groupInfo.groupObj, request, userInfo, groupInfo)
+        self.query.clear_confirmations(userInfo.id, groupInfo.id)
